fef574204825a1c446efd68e0f3ba70d2776b50b
suhunlin <lin.suhun@msa.hinet.net>
2023-08-17 15:23:22 +0800

    1.Add layout 2.Initial TextView and EditText

--- app/src/main/java/com/suhun/dbaccess0817/MainActivity.java
+++ app/src/main/java/com/suhun/dbaccess0817/MainActivity.java
@@ -3,12 +3,46 @@ package com.suhun.dbaccess0817;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView result;
+    private EditText whichIdUpdate, name, tel, birthday, whichIdDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        result = findViewById(R.id.lid_result);
+        whichIdUpdate = findViewById(R.id.lid_updateIdInput);
+        name = findViewById(R.id.lid_nameInput);
+        tel = findViewById(R.id.lid_telInput);
+        whichIdDelete = findViewById(R.id.lid_deleteIdInput);
+    }
+
+    public void queryFun(View view){
+
+    }
+
+    public void insertFun(View view){
+
+    }
+
+    public void updateFun(View view){
+
+    }
+
+    public void deleteFun(View view){
+
+    }
+
+    public void dateSelectFun(View view){
+
     }
 }
